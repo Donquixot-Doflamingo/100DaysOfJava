deffd2da0dc0d81c66052b67d75c9fb7d5be2349
Harsh Khandelwsal <007harshkhandelwal@gmail.com>
2022-02-26 12:38:10 +0530

    Update Node.java

--- Node.java
+++ Node.java
@@ -1,10 +1,1 @@
-public class Node<T> {
-    T data;
-    Node<T> next;
 
-    Node(T data){
-        this.data = data;
-        // this isn't required cause the defult value of any reference is null
-        // next = null;
-    }
-}
