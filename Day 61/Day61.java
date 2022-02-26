e03c6408f01365e475e6a2f20729f0eb8e265577
Harsh Khandelwsal <007harshkhandelwal@gmail.com>
2022-02-26 12:37:48 +0530

    Update Day61.java

--- Day 61/Day61.java
+++ Day 61/Day61.java
@@ -1,5 +1,16 @@
 import java.util.*;
 
+class Node<T> {
+    T data;
+    Node<T> next;
+
+    Node(T data){
+        this.data = data;
+        // this isn't required cause the defult value of any reference is null
+        // next = null;
+    }
+}
+
 public class Day61 {
 
     // PreRequisites
